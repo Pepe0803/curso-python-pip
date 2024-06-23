31b029f7760d70781528da370bc89a048c128c2c
PepeFar <pepe.farias.95@gmail.com>
2024-06-23 14:53:07 -0600

    Cambios en carpeta App

--- app/main.py
+++ app/main.py
@@ -2,23 +2,27 @@ import utils
 import read_csv
 import charts
 
+
 def run():
-  data = read_csv.read_csv('./app/data.csv')
-  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+    data = read_csv.read_csv('data.csv')
+    # print(data)
+    data = list(
+        filter(lambda item: item['Continent'] == 'South America', data))
+    # print(data)
 
-  countries = list(map(lambda x: x['Country'], data))
-  percentages = list(map(lambda x: x['World Population Percentage'], data))
-  charts.generate_pie_chart(countries, percentages)
-  '''
-  country = input('Type Country => ')
+    countries = list(map(lambda x: x['Country'], data))
+    percentages = list(map(lambda x: x['World Population Percentage'], data))
+    charts.generate_pie_chart(countries, percentages)
 
-  result = utils.population_by_country(data, country)
+    country = input('Type Country => ')
 
-  if len(result) > 0:
-    country = result[0]
-    labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
-  '''
+    result = utils.population_by_country(data, country)
+
+    if len(result) > 0:
+        country = result[0]
+        labels, values = utils.get_population(country)
+        charts.generate_bar_chart(country['Country'], labels, values)
+
 
 if __name__ == '__main__':
-  run()
+    run()
